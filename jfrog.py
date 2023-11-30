18bb9a16d5d39575ac8292d4cd7e5616f97a15e3
ManiDevops21 <91456961+ManiDevops21@users.noreply.github.com>
2023-11-30 10:15:06 +0800

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -3,44 +3,32 @@
 import requests
 import subprocess
 
-
 def jfrogUpload():
-    # Define the URL, file path, and authentication credentials
-    url = 'http://54.89.185.11:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
-    file_path = '/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    url = "http://54.89.185.11:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+    file_path = "/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
     username = 'admin'
-    password = 'Admin@01'  # Replace 'your password' with in the actual password
+    password = 'Admin@01'
 
-    # send the PUT request with authentication and file upload
-    with open(file_path, 'r') as file:
+    with open(file_path,'rb') as file:
         response = requests.put(url, auth=(username, password), data=file)
-
-    # check the response status code
     if response.status_code == 201:
         print("\nPUT request was successful!")
     else:
-        print(f"PUT request failed with status code {response.status_code}")
+        print(f"PUT reuquest failed with status code(response.status_code)")
         print("Response content:")
-        print("response.text")
-
+        print(response.text)
 
 def mvnBuild():
-    # Define the Maven command
-    maven_command = "mvn clean install -DskipTests"
+   maven_command = "mvn clean install -DskipTests"
 
-    # Run the Maven command as a subprocess
-    try:
-        subprocess.run(maven_command, check=True, text=True, shell=True)
-        print("\nMaven build completed successfully.")
-    except subprocess.CalledprocessError as e:
-        print(f"Error: Maven build failed with exit code {e.returncode}")
-
+try:
+    print("\nMaven build completed succesfully.")
+except subprocess.CalledProcessError as e:
+       print(f"Error: Maven build failed with exit code (e.returncode)")
 
 def main():
-    #    mvnBuild()
+    mvnBuild()
     jfrogUpload()
 
-
-##########################################################################
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
