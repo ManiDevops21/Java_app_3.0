73a51db334d7c56816de96ecfd759025cea54938
ManiDevops21 <91456961+ManiDevops21@users.noreply.github.com>
2023-11-30 08:53:35 +0800

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -3,24 +3,26 @@
 import requests
 import subprocess
 
-def jfrogupload():
+
+def jfrogUpload():
     # Define the URL, file path, and authentication credentials
     url = 'http://54.89.185.11:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.01-SNAPSHOT.jar'
     file_path = '/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.01-SNAPSHOT.jar'
     username = 'admin'
-    password = 'Admin@01' #Replace 'your password'cwith in the actual password
-    
+    password = 'Admin@01'  # Replace 'your password' with in the actual password
+
     # send the PUT request with authentication and file upload
     with open(file_path, 'rb') as file:
         response = requests.put(url, auth=(username, password), data=file)
 
     # check the response status code
-    if response.status_code ==201:
-         print("\nPUT request was successful!")
+    if response.status_code == 201:
+        print("\nPUT request was successful!")
     else:
-         print(f"PUT request failed with status code {response.status_code}")
-         print("Response content:")
-         print("response.text)
+        print(f"PUT request failed with status code {response.status_code}")
+        print("Response content:")
+        print("response.text")
+
 
 def mvnBuild():
     # Define the Maven command
@@ -29,14 +31,16 @@ def mvnBuild():
     # Run the Maven command as a subprocess
     try:
         subprocess.run(maven_command, check=True, text=True, shell=True)
-        printf("\nMaven build completed successfully.")
+        print("\nMaven build completed successfully.")
     except subprocess.CalledprocessError as e:
-        printf(f"Error: Maven build failed with exit code {e.returncode}")
+        print(f"Error: Maven build failed with exit code {e.returncode}")
+
 
 def main():
-#    mvnBuild()
-     jfrogUpload()
+    #    mvnBuild()
+    jfrogUpload()
+
 
 ##########################################################################
 if __name__ == "__main__":
-   main() 
+    main()
